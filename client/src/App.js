03a5a8da4fb16a5b6994e1f61fbdfb4a763e8663
AhmedRashad <ahmedrabie441@gmail.com>
2022-06-02 22:56:47 +0200

    fix some error

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Pages
-import HomePage from "./pages/HomePage";
 import Signin from "./pages/Login";
 import Signup from "./pages/Register";
+import Home from "./pages/home/Home";
+import CreateAccount from "./pages/createAccount/CreateAccount";
 
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/createAccount" element={<CreateAccount />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
