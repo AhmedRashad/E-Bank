bb5b55e59e19411c26402beebd5f9d1e5f55a070
Joe_Hsn <youssef.hassan.mahmoud@gmail.com>
2022-06-02 21:35:10 -0700

    Sign in/up pages are done, the missing thing the LOGO

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Pages 
+import HomePage from "./pages/HomePage";
+import Signin from "./pages/Login";
+import Signup from "./pages/Register";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">test</h1>;
+  return (<>
+     <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  </>);
 }
 
 export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
+export default function Login(props) {
+  return (
+    <>
+      <main className="min-h-screen px-4 bg-[url('images/pattern.jpg')] bg-cover">
+        <div className="min-h-screen max-w-prose bg-white mx-auto flex items-center justify-center">
+          <form className="w-4/5 space-y-8 py-4">
+            {/* Quick Login */}
+            <a
+              className="px-7 py-3 text-gray-700 border-[1px] border-gray-700 font-medium text-xs md:text-base leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3 "
+              href="#!"
+            >
+              <FcGoogle className="w-3.5 h-3.5 mr-2" /> Continue with Google
+            </a>
+            {/* Divider */}
+            <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+              <p className="text-center text-slate-500 font-semibold mx-4 mb-0">
+                OR
+              </p>
+            </div>
+
+            {/* Email input  */}
+            <div className="relative mb-6">
+              <input
+                required
+                type="text"
+                id="mail"
+                className="peer block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:bg-white focus:border-blue-600 focus:outline-none placeholder-transparent"
+                placeholder="Email address"
+                autoComplete="on"
+              />
+              <label
+                htmlFor="mail"
+                className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-blue-600 peer-placeholder-shown:text-gray-500 peer-placeholder-shown:text-xl peer-placeholder-shown:top-2 transition-all peer-placeholder-shown:pb-1
+                  peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-600"
+              >
+                Email address
+              </label>
+            </div>
+
+            {/* <!-- Password input --> */}
+            <div className="relative mb-6">
+              <input
+                required
+                type="password"
+                id="password"
+                className="peer block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none placeholder-transparent"
+                placeholder="Password"
+              />
+              <label
+                htmlFor="password"
+                className="absolute left-3 -top-2.5 bg-white px-1 text-sm text-blue-600 peer-placeholder-shown:text-gray-500 peer-placeholder-shown:text-xl peer-placeholder-shown:top-2 transition-all peer-placeholder-shown:pb-1
+                  peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-600"
+              >
+                Passowrd
+              </label>
+            </div>
+
+            {/* Check box */}
+            <div className="flex flex-wrap md:flex-nowrap justify-between items-center mb-6">
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer checked:after:content-['✓'] relative after:absolute after:top-2/4 after:left-2/4 after:-translate-x-2/4 after:-translate-y-2/4 after:text-white"
+                  id="rememberbox"
+                />
+                <label
+                  className="form-check-label inline-block text-gray-800"
+                  htmlFor="rememberbox"
+                >
+                  Remember me
+                </label>
+              </div>
+              <a
+                href="#!"
+                className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 mx-auto sm:mx-0 mt-2 sm:mt-0 transition ease-in-out"
+                aria-disabled
+              >
+                Forgot password?
+              </a>
+            </div>
+
+            {/* <!-- Submit button --> */}
+            <button
+              type="submit"
+              className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+            >
+              Sign in
+            </button>
+
+            {/* Divider */}
+            <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+              <p className="text-center text-slate-500 font-semibold mx-4 mb-0">
+                OR
+              </p>
+            </div>
+
+            {/* Quick Login */}
+            <div className="w-full text-center">You don't have account? <Link className="text-blue-500 underline" to={'/signup'}>Sign up</Link></div>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+}
