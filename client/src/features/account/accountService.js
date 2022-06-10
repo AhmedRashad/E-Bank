ebdb908cfc81ba92414902b02fd9790065d1c32d
AhmedRashad <ahmedrabie441@gmail.com>
2022-06-10 22:50:35 +0200

    add Credentials

--- client/src/features/account/accountService.js
+++ client/src/features/account/accountService.js
@@ -5,25 +5,33 @@ const API_URL = URL + "/accounts";
 
 // Get all accounts
 const getAccounts = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axios.get(API_URL, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // add new account
 const addAccount = async (account) => {
-  const response = await axios.post(API_URL, account);
+  const response = await axios.post(API_URL, account, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // update account
 const updateAccount = async (id, account) => {
-  const response = await axios.put(API_URL + "/" + id, account);
+  const response = await axios.put(API_URL + "/" + id, account, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // delete account
 const deleteAccount = async (id) => {
-  const response = await axios.delete(API_URL + "/" + id);
+  const response = await axios.delete(API_URL + "/" + id, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
--- client/src/features/user/userService.js
+++ client/src/features/user/userService.js
@@ -5,37 +5,49 @@ const API_URL = URL + "/user";
 
 // get all users
 const getAllUsers = async () => {
-  const response = await axios.get(API_URL + "/all");
+  const response = await axios.get(API_URL + "/all", {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // register new user
 const register = async (user) => {
-  const response = await axios.post(API_URL + "/register", user);
+  const response = await axios.post(API_URL + "/register", user, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // login user
 const login = async (user) => {
-  const response = await axios.post(API_URL + "/login", user);
+  const response = await axios.post(API_URL + "/login", user, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // get user by id
 const getUser = async () => {
-  const response = await axios.get(API_URL + "/me");
+  const response = await axios.get(API_URL + "/me", {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // logout user
 const logout = async () => {
-  const response = await axios.get(API_URL + "/logout");
+  const response = await axios.get(API_URL + "/logout", {
+    withCredentials: true,
+  });
   return response.data;
 };
 
 // update user
 const updateUser = async (id, status) => {
-  const response = await axios.put(API_URL + "/approve/" + id, status);
+  const response = await axios.put(API_URL + "/approve/" + id, status, {
+    withCredentials: true,
+  });
   return response.data;
 };
 
